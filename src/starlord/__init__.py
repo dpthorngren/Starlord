2ce619adb51f693cc7d8b8ec3293f8c553e482f2
Daniel Thorngren <dpthorngren@gmail.com>
2025-09-19 01:43:22 -0400

    Simple CI testing setup.

--- src/starlord/__init__.py
+++ src/starlord/__init__.py
@@ -1,3 +1,4 @@
 from starlord._version import __version__
 from starlord.star_fitter import StarFitter
 
+__all__ = ["__version__", "StarFitter"]
--- src/starlord/star_fitter.py
+++ src/starlord/star_fitter.py
@@ -3,4 +3,5 @@ import numpy as np
 
 class StarFitter():
     def __init__(self):
+        self.x = np.arange(5)
         print("Placeholder.")
