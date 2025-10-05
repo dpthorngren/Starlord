b19571ba76b5ad480781e04990de43fd98f363ce
Daniel Thorngren <dpthorngren@gmail.com>
2025-10-05 01:11:37 -0400

    :Fixed coverage and included Cython files.

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
-from setuptools import setup, Extension
-from Cython.Build import cythonize
+import os
+
 import numpy
+from Cython.Build import cythonize
+from setuptools import Extension, setup
 
-ext = Extension("starlord.cy_tools", ["src/starlord/cy_tools.pyx"], include_dirs = [numpy.get_include()])
+ext = Extension("starlord.cy_tools", ["src/starlord/cy_tools.pyx"], include_dirs=[numpy.get_include()])
 
-setup(ext_modules=cythonize([ext]))
+# Hack to detect if coverage data is being generated
+directives = {}
+if os.path.exists("./htmlcov"):
+    ext.define_macros = [("CYTHON_TRACE_NOGIL", "1")]
+    directives['linetrace'] = True
+
+setup(ext_modules=cythonize([ext], compiler_directives=directives))
